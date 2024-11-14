c538876bc7715b0b584e6f9043595c338f17971c
Phillip <phillipniakoulov@gmail.com>
2024-11-14 13:22:25 -0800

    Integrated dashboard and added visualization

--- Dashboard/src/main/java/controlPanel/DrawPanel.java
+++ Dashboard/src/main/java/controlPanel/DrawPanel.java
@@ -2,6 +2,9 @@ package controlPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
@@ -12,13 +15,28 @@ import java.beans.PropertyChangeListener;
  * @version 2.0
  */
 public class DrawPanel extends JPanel implements PropertyChangeListener {
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
+
+		// Calculate the scaling factors
+		int width = getWidth();
+		int height = getHeight();
+		double scaleX = (double) width / 100;
+		double scaleY = (double) height / 100;
+
+		// Scale and make origin the bottom left (instead of top left)
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g2d.scale(scaleX, scaleY);
+		AffineTransform transform = AffineTransform.getScaleInstance(1, -1);
+		transform.translate(0, -100); // Translate to flip around the bottom
+		g2d.transform(transform);
+
 		// loop to paint all points in a data structure
-		for (Point p : Blackboard.getInstance().getPoints()) {
-			g.fillOval(p.x, p.y, 10, 10);
+		for (Point2D p : Blackboard.getInstance().getPoints()) {
+			g2d.fill(new Ellipse2D.Double(p.getX() - 1, p.getY() - 1, 2, 2)); // Circle centered at (x1, y1)
 		}
 	}
 	
--- Dashboard/src/main/java/controlPanel/Subscriber.java
+++ Dashboard/src/main/java/controlPanel/Subscriber.java
@@ -2,12 +2,7 @@ package controlPanel;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.Socket;
+import server.SocketServer;
 
 /**
  * Client class to connect to the Meta Quest 3 and receive data.
@@ -21,50 +16,31 @@ public class Subscriber implements Runnable {
 	private String ip;
 	private int port;
 	private boolean running = false;
+	private SocketServer server;
 	
-	public Subscriber(String ip, int port) {
+	public Subscriber(String ip, int port, double[] originValues, double[] deltaXValues, double[] deltaYValues) {
 		this.ip = ip;
 		this.port = port;
+		this.server = new SocketServer(ip, port, originValues, deltaXValues, deltaYValues);
 	}
 	
 	@Override
 	public void run() {
-
-		try {
-			Socket socket = new Socket(ip, port);
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			logger.info("Subscriber is running");
-			running = true;
-			while (running) {
-				String command = in.readLine();
-				if (command != null) {
-					parse(command);
-				}
-			}
-			logger.info("Subscriber is stopping");
-			socket.close();
-		} catch (IOException e) {
-			logger.error("I/O error in Subscriber: {}", e.getMessage(), e);
-		} catch (Exception e) {
-			logger.error("Unexpected error in Subscriber: {}", e.getMessage(), e);
-		} finally {
-			running = false;
-		}
+		this.server.start();
+		logger.info("Subscriber is running");
+		this.running = true;
 	}
 	
 	public void stop() {
-		running = false;
-	}
-	
-	private void parse(String command) {
-		String[] tokens = command.split(",");
 		try {
-			int x = Integer.parseInt(tokens[0]);
-			int y = Integer.parseInt(tokens[1]);
-			Blackboard.getInstance().add(new Point(x, y));
-		} catch (NumberFormatException e) {
-			logger.error("Error parsing command", e);
+			this.server.stop();
+			this.running = false;
+			logger.info("Subscriber has stopped");
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt(); // Restore the interrupted status
+			logger.info("Failed to stop the WebSocket Server: " + e.getMessage());
 		}
+
 	}
 	
 	public boolean isRunning() {
